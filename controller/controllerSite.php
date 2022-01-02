92a4ad68d669d39217016de4970f4007efb9344f
Matthieu <matthieu.tran@universite-paris-saclay.fr>
2022-01-02 15:24:36 +0100

    creation de la fonction acceuil dans controllerSite

--- controller/controllerSite.php
+++ controller/controllerSite.php
@@ -1,6 +1,14 @@
 <?php
-
+require_once("model/Categorie.php");
+require_once("model/Commentaire.php");
+require_once("model/Famille.php");
+require_once("model/Ingredient.php");
+require_once("model/Recette.php");
+require_once("model/Ustensile.php");
+require_once("model/Utilisateur.php");
 class controllerSite
 {
-
+    public static function acceuil(){
+        require_once ("./view/acceuil.php");
+    }
 }
--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 
     require_once("controller/controllerSite.php");
 
-
     $action = "acceuil";
-    if (isset($_GET["action"]) && in_array($_GET["action"],get_class_methods("ControllerVoiture")))
+    if (isset($_GET["action"]) && in_array($_GET["action"],get_class_methods("controllerSite")))
         $action = $_GET["action"];
 
     controllerSite::$action();
