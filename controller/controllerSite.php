084aea4a5929ad1dc6c9d1195523a9a0e0eeea87
Matthieu <matthieu.tran@universite-paris-saclay.fr>
2022-01-03 09:46:49 +0100

    Ajout de la fonction creerRecette

--- controller/controllerSite.php
+++ controller/controllerSite.php
@@ -11,4 +11,26 @@ class controllerSite
     public static function acceuil(){
         require_once("./view/acceuil.php");
     }
+
+    public static function createdRecette()
+    {
+        $lesRecettes = Recette::getAllRecette();
+        extract($_GET);
+        if (!in_array($nomRecette, $lesRecettes)) {
+            Recette::addRecette($nomRecette, $difficulteRecette, $descriptionRecette, $numUtilisateur);
+            echo "Recette ajoute";
+        } else {
+            $numRecette = getNumRecettebyNomRecette($nomRecette);
+
+            /*
+             * Ajouter un href qui va pointer vers la recette
+             */
+            echo "Recette déja crée par un membre";
+        }
+        self::acceuil();
+    }
+
+
+
+
 }
--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
-
     require_once("controller/controllerSite.php");
-
     $action = "acceuil";
     if (isset($_GET["action"]) && in_array($_GET["action"],get_class_methods("controllerSite")))
         $action = $_GET["action"];
-
     controllerSite::$action();
-
 ?>
