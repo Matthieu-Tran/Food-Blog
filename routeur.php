90cea3829b5e3233726d76361e8701d357f6994c
Matthieu <matthieu.tran@universite-paris-saclay.fr>
2022-01-04 22:50:13 +0100

    petite modif routeur

--- routeur.php
+++ routeur.php
@@ -3,7 +3,7 @@
     $action = "acceuil";
     if (isset($_GET["action"]) && in_array($_GET["action"],get_class_methods("controllerSite")))
         $action = $_GET["action"];
-    require_once("view/header.html");
+    //require_once("view/header.html");
     controllerSite::$action();
     require_once("view/footer.html");
 ?>
