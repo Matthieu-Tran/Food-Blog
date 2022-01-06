0f59adbbb3f7f503e3288a90e14c21ecdc0c28bf
Matthieu <matthieu.tran@universite-paris-saclay.fr>
2022-01-06 14:11:14 +0100

    ajout de l'image

--- view/acceuil.php
+++ view/acceuil.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" type="text/css" href="view/css/acceuil.css"/>
+    <link rel="stylesheet" type="text/css" href="css/acceuil.css"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <title>Acceuil</title>
 </head>
