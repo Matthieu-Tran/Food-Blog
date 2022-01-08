53f48a1d9ec5674aadffd9e91f25124f2b8857a1
Lershu <matthieuschuler@orange.fr>
2022-01-08 16:12:44 +0100

    Version stable ( Sauf photos)

--- view/acceuil.php
+++ view/acceuil.php
@@ -1,41 +1,38 @@
  <section class="accueil bg-dark d-flex w-100 h-100 flex-column justify-content-center align-items-center">
 
-        <h1 class="display-1 text-white text-center">Faîtes vous plaisir</h1>
-        <p class="lead text-center text-white">Découvrez les recettes de nos membres et partagez nous vos plus belles recettes</p>
-        <p class="lead mb-1">
-            <a href="#debut" class="btn btn-lg btn-secondary">En savoir plus</a>
-        </p>
+     <h1 class="display-1 text-white text-center">Faîtes vous plaisir</h1>
+     <p class="lead text-center text-white">Découvrez les recettes de nos membres et partagez nous vos plus belles recettes</p>
+     <p class="lead mb-1">
+         <a href="#debut" class="btn btn-lg btn-secondary">En savoir plus</a>
+     </p>
 
-    </section>
+ </section>
 
-    <!-- Grille Responsive -->
+ <!-- Grille Responsive -->
 
-    <div id="debut" class="container py-5 bg-light">
+ <div id="debut" class="container py-5 bg-light">
 
-        <h2 class="display-6 text-center mb-5" >Commencez à vous régalez</h2>
-        <!-- 576 XS - > 576px S > 768px M > 992px L > 1200px Extra Large-->
-       <div class="row">
-           <?php
-           foreach($lesRecettes as $key=>$value){ ?>
-            <div class="col-md-4 col-sm-6">
-                <div class="card mb-4 shadow-sm">
-                    <img src="view/image/<?php echo $lesRecettes[$key]['numRecette'];?>" class="w-100">
-                    <div class="card-body">
-                        <p class="card-title">
-                            <h3><?php echo $lesRecettes[$key]['nomRecette'];?></h3>
-                        </p>
-                        <div class="btn-group">
-                            <button onclick="window.location.href='routeur.php?action=afficherRecette&numRecette=<?php echo $lesRecettes[$key]['numRecette'];?>'" type="button" class="btn btn-sm btn-outline-secondary ml-1">
-                                Découvrir
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-           <?php }
-           ?>
-        </div>
-    </div>
-
-
-
+     <h2 class="display-6 text-center mb-5">Commencez à vous régalez</h2>
+     <!-- 576 XS - > 576px S > 768px M > 992px L > 1200px Extra Large-->
+     <div class="row">
+         <?php
+            foreach ($lesRecettes as $key => $value) { ?>
+             <div class="col-md-4 col-sm-6">
+                 <div class="card mb-4 shadow-sm">
+                     <img src="view/image/<?php echo $lesRecettes[$key]['numRecette']; ?>.png" class="w-100">
+                     <div class="card-body">
+                         <p class="card-title">
+                         <h3><?php echo $lesRecettes[$key]['nomRecette']; ?></h3>
+                         </p>
+                         <div class="btn-group">
+                             <button onclick="window.location.href='routeur.php?action=afficherRecette&numRecette=<?php echo $lesRecettes[$key]['numRecette']; ?>'" type="button" class="btn btn-sm btn-outline-secondary ml-1">
+                                 Découvrir <?php echo $lesRecettes[$key]['numRecette']; ?>
+                             </button>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         <?php }
+            ?>
+     </div>
+ </div>
--- view/afficher.php
+++ view/afficher.php
@@ -1,12 +1,11 @@
 <div id="card">
     <div class="card w-75 mx-auto">
-        <img
-                src="view/image/<?php echo $numRecette?>"
-                class="card-img-top"
-        />
+        <div class="image">
+            <img src="view/image/<?php echo $numRecette ?>.png" class="card-img-top" />
+        </div>
         <div class="card-body">
-            <h5 class="card-title"><?php echo $nomRecette?></h5>
-            <h6 class="card-subtitle mb-2 text-muted">Difficulté recette: <?php echo $difficulteRecette?></h6>
+            <h5 class="card-title"><?php echo $nomRecette ?></h5>
+            <h6 class="card-subtitle mb-2 text-muted">Difficulté recette: <?php echo $difficulteRecette ?></h6>
             <p class="card-text">
                 Some quick example text to build on the card title and make up the bulk of the
                 card's content.
@@ -25,24 +24,24 @@
             <div class="tab-content" id="pills-tabContent">
                 <div class="tab-pane fade show active" id="tabs-ingredient" role="tabpanel" aria-labelledby="ingredient-tab">
                     <ul class="list-group">
-                            <?php
-                            if($nbIngredientRecette>0){
-                                for ($i =0; $i<$nbIngredientRecette; $i++){?>
-                            <li class="list-group-item">
-                                <?php echo $listeIngredient[$i]['nomIngredient'];?>
-                                <span class="badge badge-primary badge-pill"><?php echo $listeQuantite[$i]['quantite']; ?></span>
-                            </li>
-                            <?php
-                                }
-                            } ?>
+                        <?php
+                        if ($nbIngredientRecette > 0) {
+                            for ($i = 0; $i < $nbIngredientRecette; $i++) { ?>
+                                <li class="list-group-item">
+                                    <?php echo $listeIngredient[$i]['nomIngredient']; ?>
+                                    <span class="badge badge-primary badge-pill"><?php echo $listeQuantite[$i]['quantite']; ?></span>
+                                </li>
+                        <?php
+                            }
+                        } ?>
                     </ul>
                 </div>
                 <div class="tab-pane fade" id="tabs-ustensile" role="tabpanel" aria-labelledby="ustensile-tab">
 
                     <ul class="list-group">
                         <?php
-                        foreach($listeNomUstensile as $key=>$value){?>
-                            <li class="list-group-item"><?php echo $listeNomUstensile[$key];?></li>
+                        foreach ($listeNomUstensile as $key => $value) { ?>
+                            <li class="list-group-item"><?php echo $listeNomUstensile[$key]; ?></li>
                         <?php } ?>
                     </ul>
 
